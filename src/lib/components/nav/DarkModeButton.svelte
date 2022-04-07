f824f1e1d9ffb34c1654ed66170e6ebc9c6ab6d9
Michael Cullan <mjcullan@gmail.com>
2022-04-07 15:26:11 -0400

    darkmode button facelift

--- src/lib/components/nav/DarkModeButton.svelte
+++ src/lib/components/nav/DarkModeButton.svelte
@@ -16,48 +16,63 @@
 	}
 
 	.btn-container {
-        margin:auto 0px;
-        margin-bottom: 2px;
-        margin-left: 20px;
-        margin-right: 2px;
+		margin: auto 0px;
+		margin-bottom: 2px;
+		margin-left: 20px;
+		margin-right: 2px;
 		width: 50px;
 		height: 20px;
 		display: flex;
-        border: 1px solid gray;
+		border: 1px solid gray;
 		position: relative;
 		border-radius: 4px;
 		background-color: rgb(245, 245, 245);
 		transition: all 200ms ease;
-
 	}
 
 	i {
-        
 		color: #111;
 		font-size: 14px;
 		transition: all 400ms ease;
+		overflow: visible;
 	}
 
-    .fas {
-        padding-left: 4px;
-    }
+	.fas {
+		padding-left: 4px;
+	}
 
 	.active .fas {
-		transform: translateX(26px);
+		transform: translateX(29px);
 		color: #fff;
+		overflow: visible;
+	}
+
+	.fas::after {
+		padding: 2px 4px 0px 1px;
+		margin: 0px -4px;
+		border-radius: 2px;
 	}
 
 	:not(.active) .fas::after {
 		content: '🌞';
+		background-color: rgb(222, 211, 211);
+		border-color: rgba(48, 51, 87, 0.7);
 	}
 
 	.active .fas::after {
 		content: '🌙';
+		background-color: rgb(24, 19, 70);
+		border-color: rgba(48, 51, 87, 0.7);
 	}
 
 	.active {
-		background-color: rgb(64, 66, 91, 0.7);
+		background-color: rgba(48, 51, 87, 0.7);
 		border-color: rgb(64, 66, 91, 0.7);
-		box-shadow: -1px 2px 10px 0px rgba(255, 255, 255, 0.2);
+		box-shadow: -1px 4px 10px 5px rgba(107, 89, 89, 0.2);
+	}
+
+	div:not(.active) {
+		background-color: rgba(233, 233, 237, 0.7);
+		box-shadow: -1px 4px 10px 5px rgba(107, 89, 89, 0.2);
 	}
 </style>
